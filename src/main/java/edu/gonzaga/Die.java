8beb53245dbacb3de4c1a2976c1d7e3ef35e7e70
Jesse Adams <62229203+jdotadot9@users.noreply.github.com>
2022-04-24 16:35:52 -0700

    Add files via upload

--- src/main/java/edu/gonzaga/Die.java
+++ src/main/java/edu/gonzaga/Die.java
@@ -1,81 +1,68 @@
-/* (C)2021 */
 package edu.gonzaga;
-
-/*
-* Class for a Die used in Yahtzee.
-*/
 
 import java.util.Random;
 
-/** Class to store the state of a single die. */
-public class Die implements Comparable<Die> {
-    private Integer sideUp; // Current die 'value' in range 1..numSides
-    private Integer numSides; // Sides on the die (should be 1...INF integer)
-    private static final Integer DEFAULT_NUM_SIDES = 6;
-    private static final Integer DEFAULT_SIDE_UP = 1;
+public final class Die
+{
+    private int numSides;
+    private int sideUp;
+    private Random rand = new Random();
 
-    public Die() {
-        this.numSides = DEFAULT_NUM_SIDES;
-        this.sideUp = DEFAULT_SIDE_UP;
-    }
-
-    public Die(Integer numSides) {
+    /**
+     * Constructor for a Die class object, sets dieValue to 
+     * to the value of the constant SIDE_UP
+     * 
+     * @param N/A
+     * @returns N/A
+     */
+    public Die(int numSides)
+    {
         this.numSides = numSides;
-        this.sideUp = DEFAULT_SIDE_UP;
-    }
-
-    public Die(Integer numSides, Integer startingSide) {
-        this.numSides = numSides;
-        this.sideUp = startingSide;
-    }
-
-    /** Rolls the die once, getting new random value. */
-    public void roll() {
-        Random rand = new Random();
-        this.sideUp = rand.nextInt(this.numSides) + 1;
+        this.sideUp = 1;
     }
 
     /**
-    * Returns current die value (the side that's up).
-    *
-    * @return Integer Current Die's Side Up
-    */
-    public Integer getSideUp() {
+     * Setter for the numSides field.
+     * 
+     * @param newNumSides
+     * @return void
+     */
+    public void setNumSides(int newNumSides)
+    {
+        this.numSides = newNumSides;
+    }
+
+    /**
+     * Getter for dieValue attribute
+     * 
+     * @param void
+     * @return this.dieValue
+     */
+    public int getDieValue()
+    {
         return this.sideUp;
     }
 
     /**
-    * Returns quantity of sides on the die.
-    *
-    * @return Integer number of sides on the die
-    */
-    public Integer getNumSides() {
-        return this.numSides;
+     * Setter for sideUp attribute.
+     * 
+     * @param newValue - value to set sideUp to.
+     * @return void
+     */
+    public void setSideUp(int newValue)
+    {
+        this.sideUp = newValue;
     }
 
     /**
-    * Provides the ability to convert the Die object into a string. representation, both with
-    * .toString(), but also in System.out.println()
-    *
-    * @return String of whatever you want this die to say for itself
-    */
-    @Override
-    public String toString() {
-        String ret = "";
-        // ret += "Die: " + this.sideUp.toString() + " of " + this.numSides.toString() + " sides";
-        ret += this.sideUp.toString();
-        return ret;
-    }
-
-    /**
-    * Makes two dice comparable using <, ==, >, etc. based on sideUp values.
-    *
-    * @param otherDie The die we're comparing to this one (two objects)
-    * @return int -1, 0, 1 for less than, equal, greater than
-    */
-    @Override
-    public int compareTo(Die otherDie) {
-        int myvar_aslkdfj = 2;
-        return this.sideUp.compareTo(otherDie.sideUp);
+     * Rolls the die by generating a new random number and
+     * assigning dieValue to that new random number
+     * 
+     * @param void
+     * @return void
+     */
+    public void rollDie()
+    {
+        this.sideUp = this.rand.nextInt(numSides) + 1;
     }
 }
