24df60b48ca45244b366606880a00bd69df22b89
Katie <kimhof@zagmail.gonzaga.edu>
2022-04-24 18:43:12 -0700

    Added more methods

--- src/main/java/edu/gonzaga/MainGUI.java
+++ src/main/java/edu/gonzaga/MainGUI.java
@@ -1,13 +1,21 @@
 package edu.gonzaga;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainGUI {
 
     JFrame mainWindow;
+    public int result;
+
+    ArrayList<PlayerScoreStatus> playersArray;
 
     public MainGUI() {
         // Frame
@@ -17,22 +25,11 @@ public class MainGUI {
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.getContentPane().setBackground(Color.black);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         // Image
         ImageIcon yahtzeeIcon = new ImageIcon("./YahtzeeMedia/Misc/pumpkin.jpg");
         mainWindow.setIconImage(yahtzeeIcon.getImage());
 
         // Start Screen
-=======
-        ImageIcon yahtzeeIcon = new ImageIcon("./YahtzeeMedia/Misc/pumpkin.jpg");
-        mainWindow.setIconImage(yahtzeeIcon.getImage());
-=======
->>>>>>> 6353d56165fa20f19bb900dc5443fbfc4db489ed
-
-
-
->>>>>>> 9cd1250de0916ec1f14b27869053037ac82c5c64
         mainWindow.setVisible(true);
         startScreen();
     }
@@ -43,9 +40,14 @@ public class MainGUI {
         myPanel.setBackground(Color.orange);
         mainWindow.add(myPanel);
 
+
         // Image
-        ImageIcon pumpkinLogo = new ImageIcon("YahtzeeMedia/Logos/pumpking.jpeg");
-        myPanel.add(new JLabel(pumpkinLogo));
+        //ImageIcon pumpkinLogo = new ImageIcon("./YahtzeeMedia/Logos/pumpking.jpeg");
+        //JLabel myLabel = new JLabel();
+        //myLabel.setIcon(pumpkinLogo);
+        //myPanel.add(myLabel);
+        //myPanel.add(new JLabel(pumpkinLogo));
+
 
         // Button
         JButton playButton = new JButton("Play");
@@ -55,18 +57,65 @@ public class MainGUI {
         Dimension playButtonSize = playButton.getPreferredSize();
         playButton.setBounds(330, 350, playButtonSize.width, playButtonSize.height);
 
-
-
         // Action Listener for Button
         playButton.addActionListener(
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e){
-                        // TODO: start playing game
+                        myPanel.setVisible(false);
+                        showRules();
                     }
                 }
         );
 
         myPanel.setVisible(true);
+    }
+
+    void showRules() {
+        JPanel myPanel = new JPanel();
+        mainWindow.add(myPanel);
+
+        JButton confirmEntry = new JButton("Play Game!");
+        myPanel.add(confirmEntry);
+
+        confirmEntry.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        myPanel.setVisible(false);
+                        selectPlayerAmt();
+                    }
+                }
+        );
+    }
+
+    void selectPlayerAmt() {
+        JPanel myPanel = new JPanel();
+        mainWindow.add(myPanel);
+
+        JComboBox selectNumPlayers = new JComboBox();
+        for(int i = 1; i <= 4; i++) {
+            selectNumPlayers.addItem(i);
+        }
+        myPanel.add(selectNumPlayers);
+
+        JButton confirmEntry = new JButton("Enter");
+        myPanel.add(confirmEntry);
+
+
+        // Action Listener for Button
+        confirmEntry.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        result = Integer.parseInt((String) selectNumPlayers.getSelectedItem());
+                    }
+                }
+        );
+    }
+
+    void createScorecards() {
+        for (int i = 1; i <= result; i++){
+            //PlayerScoreStatus - make new player
+            //add player to player array list
+        }
     }
 
 }
--- src/main/java/edu/gonzaga/Yahtzee.java
+++ src/main/java/edu/gonzaga/Yahtzee.java
@@ -6,12 +6,6 @@ public class Yahtzee
 {
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame("Yahtzee!");
-
-        frame.setBounds(50, 50, 400, 450);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.add(new HandPanel(10, 10));
-        frame.setVisible(true);
+        MainGUI tempGUI = new MainGUI();
     }
 }
