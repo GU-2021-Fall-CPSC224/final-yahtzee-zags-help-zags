8a6f59ea042a7e40b61c453cfb6811eb86e0b159
Jackie <jramsey@zagmail.gonzaga.edu>
2022-04-25 16:31:25 -0700

    more tests

--- src/test/java/edu/gonzaga/PlayerScoreStatusTest.java
+++ src/test/java/edu/gonzaga/PlayerScoreStatusTest.java
@@ -1,4 +1,32 @@
 package edu.gonzaga;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlayerScoreStatusTest {
+
+    @Test
+    void scorePlayerTest() {
+        int playerNum = 1;
+        PlayerScoreStatus scorePlayer = new PlayerScoreStatus(playerNum);
+        assertEquals(playerNum, scorePlayer.getPlayerNum());
+    }
+
+    @Test
+    void scorePlayercase2Test() {
+        int playerNum = 4;
+        PlayerScoreStatus scorePlayer = new PlayerScoreStatus(playerNum);
+        assertEquals(playerNum, scorePlayer.getPlayerNum());
+    }
+
+    @Test
+    void scorePlayercase3Test() {
+        int playerNum = 3;
+        PlayerScoreStatus scorePlayer = new PlayerScoreStatus(playerNum);
+        assertEquals(playerNum, scorePlayer.getPlayerNum());
+    }
+    
 }
