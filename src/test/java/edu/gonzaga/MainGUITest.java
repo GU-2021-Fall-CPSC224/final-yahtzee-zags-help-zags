84249e6dc871971d94335706a6106eda37bccb13
Katie <kimhof@zagmail.gonzaga.edu>
2022-04-25 15:37:51 -0700

    Added a few more

--- src/test/java/edu/gonzaga/MainGUITest.java
+++ src/test/java/edu/gonzaga/MainGUITest.java
@@ -1,4 +1,9 @@
 package edu.gonzaga;
 
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainGUITest {
+
 }
