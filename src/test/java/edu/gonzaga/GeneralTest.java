c8ef32c922e5f416c89604fe11850fe872882c84
Jesse Adams <jesseaeadams@gmail.com>
2022-04-24 17:21:20 -0700

    folder added

--- src/test/java/edu/gonzaga/GeneralTest.java
+++ src/test/java/edu/gonzaga/GeneralTest.java
@@ -1,14 +1,14 @@
-package edu.gonzaga;
+// package edu.gonzaga;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
 
 
-public class GeneralTest {
-    @Test
-    void alwaysTrue() {
-        Assertions.assertTrue(true);
-    }
-}
+// public class GeneralTest {
+//     @Test
+//     void alwaysTrue() {
+//         Assertions.assertTrue(true);
+//     }
+// }
